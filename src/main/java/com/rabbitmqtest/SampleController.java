249710c82ea3b5d7138e9d5525fa0440dc7aef0d
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-10-11 20:10:09 +0900

    🐇 :: RabbitMQ

---
+++ src/main/java/com/rabbitmqtest/SampleController.java
@@ -0,0 +1,20 @@
+package com.rabbitmqtest;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+    private static final String EXCHANGE_NAME = "sample.exchanges";
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @GetMapping("/sample/queue")
+    public String samplePublish() {
+        rabbitTemplate.convertAndSend(EXCHANGE_NAME, "sample.khcho.#", "RabbitMQ + Springboot = Success!");
+        return "message sending";
+    }
+}
